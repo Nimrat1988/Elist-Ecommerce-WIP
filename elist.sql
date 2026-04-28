12a98cce9e83a2e7c99d9c722939d9290c6f7fca
Nimrat Pal Kaur <kaur.nimrat2023@gmail.com>
2026-04-28 10:54:31 -0600

    Update elist.sql

--- elist.sql
+++ elist.sql
@@ -1,1 +1,6 @@
+-- Questions and queries from the operations team
 
+--What is the date of the earliest and latest order, returned in one query?
+--select min and max of purchase_ts from the table orders.
+SELECT min(purchase_ts), max(purchase_ts)
+FROM core.orders
